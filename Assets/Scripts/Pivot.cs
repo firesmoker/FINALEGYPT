089b1f875855bba39f2f95e89ef74323671543f9
BurnEmDown <amitreuveni@gmail.com>
2020-01-07 23:23:47 +0200

    Fixed player dancing bug

--- Assets/Scripts/Pivot.cs
+++ Assets/Scripts/Pivot.cs
@@ -19,6 +19,11 @@ public class Pivot : MonoBehaviour
 
         difference.Normalize();
 
+        if (difference.x < 0.1 && difference.x > -0.1) // Check if mouse is very close to player
+        {
+            return; // If it is, skip the pivoting and wait for the mouse to be a little further away from the player
+        }
+
         rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
